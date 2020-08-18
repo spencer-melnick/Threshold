18064edb734fe97bca2fcf2df0aba9375792283d
Spencer Melnick <smelnick97@gmail.com>
2020-08-18 11:40:13 -0400

    Enabling gameplay abilities

--- Source/Threshold/Threshold.Build.cs
+++ Source/Threshold/Threshold.Build.cs
@@ -8,6 +8,7 @@ public class Threshold : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore",
+			"HeadMountedDisplay", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 	}
 }
