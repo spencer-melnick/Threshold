1d6d518df1b930318e2b4895ed5a23f34ae97c52
Spencer Melnick <smelnick97@gmail.com>
2020-08-21 19:43:01 -0400

    Removing duplicate files and readding GAS dependencies

--- Source/Threshold/Threshold.Build.cs
+++ Source/Threshold/Threshold.Build.cs
@@ -8,6 +8,6 @@ public class Threshold : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks", "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
 	}
 }
