6c59ecc71f6033fde2211d55ed2496471a8822bc
Spencer Melnick <smelnick97@gmail.com>
2020-09-10 16:27:40 -0400

    Adding hit slowdown ability task

---
+++ Source/Threshold/Abilities/Tasks/AT_ApplySlowdownCurve.h
@@ -0,0 +1,58 @@
+﻿// Copyright (c) 2020 Spencer Melnick
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AT_ApplySlowdownCurve.generated.h"
+
+
+
+// Forward declarations
+
+class UAbilitySystemComponent;
+
+
+
+// Delegate declaration
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FApplySlowdownCurveDelegate);
+
+/**
+ * Applies a play rate change to the active montage from a float curve
+ */
+UCLASS()
+class UAT_ApplySlowdownCurve : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+	UAT_ApplySlowdownCurve();
+
+	static UAT_ApplySlowdownCurve* ApplySlowdownCurve(UGameplayAbility* OwningAbility, FName TaskInstanceName, UCurveFloat* SlowdownCurve, float Duration);
+
+	
+	// Ability task overrides
+
+	virtual void Activate() override;
+	virtual void TickTask(float DeltaTime) override;
+	virtual void PreDestroyFromReplication() override;
+	virtual void OnDestroy(bool bInOwnerFinished) override;
+
+	// Delegates
+
+	UPROPERTY(BlueprintAssignable)
+	FApplySlowdownCurveDelegate OnEnd;
+
+
+protected:
+	UPROPERTY()
+	UCurveFloat* SlowdownCurve;
+	float Duration;
+
+
+private:
+	// Private execution variables
+	
+	float ElapsedTime = 0.f;
+	bool bIsFinished = true;
+};
