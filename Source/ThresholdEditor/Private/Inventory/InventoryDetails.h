39ad54f9827056f8b0ecf3bb29ce86e1dd80460e
Spencer Melnick <smelnick97@gmail.com>
2020-09-25 13:19:52 -0400

    Removing interface display

--- Source/ThresholdEditor/Private/Inventory/InventoryDetails.h
+++ Source/ThresholdEditor/Private/Inventory/InventoryDetails.h
@@ -23,8 +23,4 @@ public:
 	
 	virtual void CustomizeHeader(::TSharedRef<IPropertyHandle, ESPMode::Fast> PropertyHandle, FDetailWidgetRow& HeaderRow, IPropertyTypeCustomizationUtils& CustomizationUtils) override;
 	virtual void CustomizeChildren(::TSharedRef<IPropertyHandle, ESPMode::Fast> PropertyHandle, IDetailChildrenBuilder& ChildBuilder, IPropertyTypeCustomizationUtils& CustomizationUtils) override;
-
-	static void AddCheckBoxDisplay(const FString& Name, bool bChecked, IDetailGroup& DetailGroup, IPropertyTypeCustomizationUtils& CustomizationUtils);
-	static void AddTextDisplay(const FString& Name, const FText& Text, IDetailGroup& DetailGroup, IPropertyTypeCustomizationUtils& CustomizationUtils);
-	static void AddInterfaceProperties(FInventoryItemHandle& ItemHandle, IDetailChildrenBuilder& ChildBuilder, IPropertyTypeCustomizationUtils& CustomizationUtils);
 };
