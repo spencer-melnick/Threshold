503d1508a307faa92a979c4d4ddf94f645f7e6c0
Spencer Melnick <smelnick97@gmail.com>
2020-10-08 19:53:16 -0400

    Experimenting with rendering preview images

---
+++ Source/ThresholdUI/Private/ThresholdUI/Actors/PreviewCapture.cpp
@@ -0,0 +1,85 @@
+﻿// Copyright (c) 2020 Spencer Melnick
+
+#include "ThresholdUI/Actors/PreviewCapture.h"
+#include "Components/SceneCaptureComponent2D.h"
+#include "Engine/TextureRenderTarget2D.h"
+
+
+// APreviewCapture
+
+
+// Component name constants
+
+FName APreviewCapture::RootComponentName(TEXT("RootSceneComponent"));
+FName APreviewCapture::CaptureComponentName(TEXT("CaptureComponent"));
+
+
+
+// Default constructor
+
+APreviewCapture::APreviewCapture()
+{
+	// Create root component
+	OriginSceneComponent = CreateDefaultSubobject<USceneComponent>(RootComponentName);
+	RootComponent = OriginSceneComponent;
+	
+	// Create default capture component
+	CaptureComponent = CreateDefaultSubobject<USceneCaptureComponent2D>(CaptureComponentName);
+	CaptureComponent->SetupAttachment(RootComponent);
+
+	// Enable rendering transparency
+	CaptureComponent->bConsiderUnrenderedOpaquePixelAsFullyTranslucent = true;
+	CaptureComponent->CaptureSource = SCS_SceneColorHDR;
+
+	// Only render actors added to the "show" list
+	CaptureComponent->PrimitiveRenderMode = ESceneCapturePrimitiveRenderMode::PRM_UseShowOnlyList;
+
+	// Disable rendering every frame
+	CaptureComponent->bCaptureEveryFrame = false;
+}
+
+
+
+// Render controls
+
+void APreviewCapture::RenderActor(AActor* TargetActor, UTextureRenderTarget2D* RenderTarget)
+{
+	if (!TargetActor || !RenderTarget)
+	{
+		return;
+	}
+
+	// Position the actor so it takes up the whole screen
+	const float AspectRatio = static_cast<float>(RenderTarget->SizeX) / static_cast<float>(RenderTarget->SizeY);
+	PositionActor(TargetActor, AspectRatio);
+
+	// Set the render target and capture the scene
+	CaptureComponent->ShowOnlyActors.Add(TargetActor);
+	CaptureComponent->TextureTarget = RenderTarget;
+	CaptureComponent->CaptureScene();
+}
+
+
+
+// Helper functions
+
+void APreviewCapture::PositionActor(AActor* TargetActor, float AspectRatio)
+{
+	// Attach the target actor
+	TargetActor->AttachToActor(this, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+	TargetActor->SetActorScale3D(FVector::OneVector);
+
+	// Find the bounding box for the object
+	FVector BoundsOrigin;
+	FVector BoundsExtent;
+	TargetActor->GetActorBounds(false, BoundsOrigin, BoundsExtent);
+
+	// Calculate the size of the bounding sphere
+	FMinimalViewInfo CaptureViewInfo;
+	CaptureComponent->GetCameraView(0.f, CaptureViewInfo);
+	CaptureViewInfo.AspectRatio = AspectRatio;
+	const float BoundingScreenSize = ComputeBoundsScreenSize(BoundsOrigin, BoundsExtent.Size(), CaptureViewInfo.Location, CaptureViewInfo.CalculateProjectionMatrix());
+
+	// Scale the actor so it takes up the whole screen
+	TargetActor->SetActorScale3D(FVector::OneVector * FillAmount / BoundingScreenSize);
+}
---
+++ Source/ThresholdUI/Public/ThresholdUI/Actors/PreviewCapture.h
@@ -0,0 +1,71 @@
+﻿// Copyright (c) 2020 Spencer Melnick
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PreviewCapture.generated.h"
+
+
+// Forward declarations
+
+class USceneCaptureComponent2D;
+
+
+
+/**
+ * Actor used to render 3D previews of other actors (such as inventory item models) to a render target
+ */
+UCLASS(Blueprintable)
+class THRESHOLDUI_API APreviewCapture : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APreviewCapture();
+
+
+
+	// Render controls
+
+	/**
+	 * Render an actor to the specified render target, attempting to fit the screen size.
+	 * Actors rendered with this function will become owned by the preview capture actor and should no longer be visible
+	 * in the scene. The render will only utilize lighting from the third lighting channel.
+	 * @param TargetActor - Actor to be rendered
+	 * @param RenderTarget - Render target where the preview will be rendered to
+	 */
+	UFUNCTION(BlueprintCallable, Category=PreviewCapture)
+	void RenderActor(AActor* TargetActor, UTextureRenderTarget2D* RenderTarget);
+
+	
+
+	// Component name constants
+
+	static FName RootComponentName;
+	static FName CaptureComponentName;
+
+
+
+	// Editor properties
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=PreviewCatpure)
+	float FillAmount = 0.5f;
+
+
+protected:
+
+	// Helper functions
+
+	void PositionActor(AActor* TargetActor, float AspectRatio);
+
+
+private:
+
+	// Components
+
+	UPROPERTY(Category=PreviewCapture, VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess="true"))
+	USceneComponent* OriginSceneComponent;
+	
+	UPROPERTY(Category=PreviewCapture, VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess="true"))
+	USceneCaptureComponent2D* CaptureComponent;
+};
