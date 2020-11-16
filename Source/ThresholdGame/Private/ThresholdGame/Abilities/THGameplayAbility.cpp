1e7cf65d3c8d28d7a13524ddb2bf77dce5fb399d
Spencer Melnick <smelnick97@gmail.com>
2020-11-16 15:32:30 -0500

    Improving tag look ahead method
    Modified DoesAbilitySatisfyTagRequirements instead of applying tags to the ability system component in CanActivateAbility (potentially triggering unwanted events)

--- Source/ThresholdGame/Private/ThresholdGame/Abilities/THGameplayAbility.cpp
+++ Source/ThresholdGame/Private/ThresholdGame/Abilities/THGameplayAbility.cpp
@@ -3,6 +3,7 @@
 #include "ThresholdGame/Abilities/THGameplayAbility.h"
 #include "ThresholdGame/Abilities/THAbilitySystemComponent.h"
 #include "ThresholdGame/Character/Animation/AnimNotifyState_LooseTag.h"
+#include "AbilitySystemGlobals.h"
 
 
 
@@ -12,32 +13,46 @@ UTHGameplayAbility::UTHGameplayAbility()
 	// Set defaults
 }
 
-bool UTHGameplayAbility::CanActivateAbility(
-	const FGameplayAbilitySpecHandle Handle,
-	const FGameplayAbilityActorInfo* ActorInfo,
+bool UTHGameplayAbility::DoesAbilitySatisfyTagRequirements(
+	const UAbilitySystemComponent& AbilitySystemComponent,
 	const FGameplayTagContainer* SourceTags,
 	const FGameplayTagContainer* TargetTags,
 	FGameplayTagContainer* OptionalRelevantTags) const
 {
-	check(ActorInfo);
-	
-	UAbilitySystemComponent* AbilitySystemComponent = ActorInfo->AbilitySystemComponent.Get();
-
-	if (!AbilitySystemComponent)
+	FGameplayTagContainer RelevantTags;
+	if (!OptionalRelevantTags)
 	{
-		return false;
+		OptionalRelevantTags = &RelevantTags;
+	}
+	
+	const bool bResult = Super::DoesAbilitySatisfyTagRequirements(AbilitySystemComponent, SourceTags, TargetTags, OptionalRelevantTags);
+	
+	if (bResult)
+	{
+		// If we succeeded, don't bother checking anything! No such thing as false positive here
+		return true;
 	}
 
+	const FGameplayAbilityActorInfo* ActorInfo = AbilitySystemComponent.AbilityActorInfo.Get();
+	check(ActorInfo);
+	const bool bServerPlayback = !ActorInfo->IsLocallyControlled() && ActorInfo->IsNetAuthority();
 	UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance();
-	UAnimMontage* AnimMontage = AbilitySystemComponent->GetCurrentMontage();
-	const bool bIsAuthority = ActorInfo->IsNetAuthority();
-	const bool bIsLocallyControlled = ActorInfo->IsLocallyControlled();
-	const bool bShouldLookForwardMontage = AnimMontage != nullptr && AnimInstance != nullptr && bIsAuthority && !bIsLocallyControlled;
-
-	FGameplayTagContainer PredictedTags;
+	UAnimMontage* AnimMontage = AbilitySystemComponent.GetCurrentMontage();
 	
-	if (bShouldLookForwardMontage)
+	if (!bServerPlayback || !AnimInstance || !AnimMontage)
 	{
+		// Skip prediction if we're not replaying a client's actions, or the animation data isn't valid
+		return bResult;
+	}
+
+	// Look ahead for activation tags if that's the only reason activation failed
+	const UAbilitySystemGlobals& AbilitySystemGlobals = UAbilitySystemGlobals::Get();
+	if (OptionalRelevantTags->Num() == 1 && OptionalRelevantTags->HasTagExact(AbilitySystemGlobals.ActivateFailTagsMissingTag))
+	{
+		// Start with all of our current tags
+		FGameplayTagContainer PredictedTags;
+		AbilitySystemComponent.GetOwnedGameplayTags(PredictedTags);
+	
 		// Look for future anim notifies based on the cvar time
 		const float CurrentMontageTime = AnimInstance->Montage_GetPosition(AnimMontage);
 		const float LookForwardTime = IConsoleManager::Get().FindConsoleVariable(TEXT("th.TagLookForwardTime"))->GetFloat();
@@ -47,28 +62,23 @@ bool UTHGameplayAbility::CanActivateAbility(
 		for (FAnimNotifyEventReference NotifyReference : NotifyReferences)
 		{
 			UAnimNotifyState_LooseTag* AnimNotifyState = Cast<UAnimNotifyState_LooseTag>(NotifyReference.GetNotify()->NotifyStateClass);
-			
 			if (!AnimNotifyState)
 			{
 				continue;
 			}
-
-			// Apply our tags to the ability system component
-			AbilitySystemComponent->AddLooseGameplayTags(AnimNotifyState->AppliedTags);
 			PredictedTags.AppendTags(AnimNotifyState->AppliedTags);
+		}
+
+		if (PredictedTags.HasAll(ActivationRequiredTags))
+		{
+			// If our predicted tags fill in all missing tags, ability activation should proceed
+			return true;
 		}
 	}
 
-	const bool Result = Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
-
-	if (bShouldLookForwardMontage)
-	{
-		// Remove our predicted tags
-		AbilitySystemComponent->RemoveLooseGameplayTags(PredictedTags);
-	}
-
-	return Result;
+	return bResult;
 }
+
 
 
 void UTHGameplayAbility::SendTargetDataToServer(const FGameplayAbilityTargetDataHandle& TargetData)
